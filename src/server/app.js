3a985021bf259ec521b3529f3a6b896e192b5026
flouheforst <flouheforst>
2020-07-16 14:13:04 +0300

    config module

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,15 @@
-const express = require("express");
-                require('dotenv').config();
+const   express       = require("express"),
+        session       = require("express-session"),
+        bodyParser    = require("body-parser"),
+        passport      = require("passport"),
+        bcrypt        = require("bcrypt"),
+        flash         = require("flash");
+
+
+
+
+// сделать класс configure
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,7 +24,24 @@ app.use('/css', express.static(process.cwd() + "/public/assets/css"));
 app.use('/img', express.static(process.cwd() + "/public/assets/img"));
 app.use('/font', express.static(process.cwd() + "/public/assets/font"));
 
+app.use(bodyParser.urlencoded({ extended : false }));
+
+app.use(session({ 
+    secret : process.env.SESSION_KEY,
+    resave : false,
+    saveUninitialized: true,
+    cookie : {
+        maxAge : 1 * 1000 * 60 * 60 * 0.1 // 10 min 
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
 app.use("/", routes);
+
+
 
 app.listen(PORT, () => {
     console.log(`Server was started on ${PORT} port`)
