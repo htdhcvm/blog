96fef242b3a7818e2c711528cf90884d05c5c730
flouheforst <flouheforst>
2020-07-24 14:08:22 +0300

    tripping unecessary images for page

--- src/js/main.js
+++ src/js/main.js
@@ -4,20 +4,4 @@ import "../static/logo-dark.png";
 import "../font/Playfair_Display/PlayfairDisplay-Regular.ttf"
 import "../font/Dosis/Dosis-Regular.ttf";
 import "../font/Montserrat/Montserrat-Regular.ttf";
-import "../img/1.jpg";
-import "../img/2.jpg";
-import "../img/3.jpg";
-import "../img/4.jpg";
-import "../img/5.jpg";
-import "../img/6.jpg";
-import "../img/7.jpg";
-import "../img/8.jpg";
-import "../img/9.jpg";
-import "../img/10.jpg";
-import "../img/11.jpg";
-import "../img/12.jpg";
-import "../img/13.jpg";
-import "../img/14.jpg";
-import "../img/15.jpg";
-
 import "./grid/MasonryGrid";
