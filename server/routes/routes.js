d30bf61817d222c0e911f9f70f76f05eb3b8712c
flouheforst <flouheforst>
2020-07-31 14:06:11 +0300

    new routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,40 +1,61 @@
-                      require('dotenv').config();
-const express       = require("express");
-const multer        = require("../configurationModules/Multer");
-const passport      = require("../configurationModules/Passport");
-const controller    = require("../controller/Controller");
+                        require('dotenv').config();
+const   express       = require("express"),
+        multer        = require("../configurationModules/Multer"),
+        passport      = require("../configurationModules/Passport"),
+        controller    = require("../controller/Controller");
 
 const router = express.Router();
 
-router.post("/create-post", multer.single("image"), controller.createPost)
+router.get("/", controller.renderMain);
+
+router.get("/search", controller.renderSearch);
+
+router.get("/settings/:login", controller.checkAuth, controller.renderSettings);
+
+router.get("/chat", controller.renderChat);
+
+router.get("/post/:hash", controller.renderPostHash);
+
+router.get("/write-post", controller.checkAuth, controller.renderWritePost);
+
+router.get("/logout", controller.checkAuth, controller.renderLogout)
+
+router.get("/signin", controller.renderSignIn);
+
+router.get("/signup", controller.renderSignUp);
+
+router.get("/user/:login", controller.renderUser);
+
+router.post("/create-comment", controller.createComment);
+
+router.post("/create-post", multer.single("image"), controller.createPost);
 
 router.post("/auth", passport.pass.authenticate("local-signin",  {successRedirect: '/', failureRedirect: "/signin",  failureFlash: true})); 
 
 router.post("/registration", passport.pass.authenticate("local-signup", {successRedirect: '/', failureRedirect: "/signup",  failureFlash: true}));
 
-function checkAuth(req, res, next){
-    if(req.isAuthenticated()) {
-        return next();
-    }
-    return res.redirect("/")
-}
+router.post("/settings/changePassword", controller.checkAuth, controller.changePassword );
 
-router.get("/search", controller.renderSearch);
+router.post("/settings/updateProfile", controller.checkAuth, controller.updateProfile );
 
-router.get("/settings", controller.renderSettings);
 
-router.get("/chat", controller.renderChat);
 
-router.get("/", controller.renderMain);
+router.get("/auth-admin", controller.renderAuthAdmin);
 
-router.get("/post/:hash", controller.renderPostHash);
+router.post("/auth-admin", passport.pass.authenticate("local-signin-admin", {successRedirect: '/admin', failureRedirect: "/auth-admin",  failureFlash: true}));
 
-router.get("/write-post", checkAuth, controller.renderWritePost);
+router.get("/admin", controller.checkAuthAdmin, controller.renderAdmin);
 
-router.get("/logout", checkAuth, controller.renderLogout)
+router.get("/admin/post", controller.checkAuthAdmin, controller.renderAdminPost);
 
-router.get("/signin", controller.renderSignIn);
+router.get("/admin/post/:hash", controller.checkAuthAdmin, controller.renderAdminPostHash);
 
-router.get("/signup", controller.renderSignUp);
+router.post("/admin/post/changeStatus", controller.checkAuthAdmin, controller.renderAdminChangeStatusPost);
+
+router.get("/admin/category", controller.checkAuthAdmin, controller.renderAdminCategory);
+
+router.post("/admin/category/add", controller.checkAuthAdmin, controller.categoryAdd);
+
+router.post("/admin/category/delete", controller.checkAuthAdmin, controller.categoryDelete);
 
 module.exports = router;
